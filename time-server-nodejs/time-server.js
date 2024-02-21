41579fdaf402ee1f88af5fdc3b18743526e66124
FANIEL ABRAHAM <fanuelsamsom44@gmail.com>
2024-02-20 18:32:13 -0800

    added project 2

--- time-server-nodejs/time-server.js
+++ time-server-nodejs/time-server.js
@@ -1,30 +1,11 @@
+const os = require("os");
 const http = require("http");
 
-function zeroFill(i) {
-  return (i < 10 ? "0" : "") + i;
-}
-
-function now() {
-  const d = new Date();
-  return {
-    year: d.getFullYear(),
-    month: zeroFill(d.getMonth() + 1),
-    date: zeroFill(d.getDate()),
-    hour: zeroFill(d.getHours()),
-    minute: zeroFill(d.getMinutes()),
-  };
-}
-
-const server = http.createServer((req, res) => {
-  if (req.url === "/api/currenttime") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-
-    res.end(JSON.stringify(now()) + "\n");
-  } else {
-    res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("404 Not Found\n");
-  }
-});
-
-server.listen(Number(process.argv[2]));
-console.log("Node server running....");
+console.log("Kubia server starting...");
+var handler = function (request, response) {
+  console.log("Received request from " + request.connection.remoteAddress);
+  response.writeHead(200);
+  response.end("You've hit " + os.hostname() + "\n");
+};
+var www = http.createServer(handler);
+www.listen(80);
